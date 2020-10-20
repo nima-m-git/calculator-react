5acc8ab1964a2686bb3e6317e3ad96628820f014
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-10-20 13:46:28 -0400

    calculator component initialized, operations added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      new: null,
+      operation: null,
+    }
+
+
+  }
+}
+
+export default Calculator;
