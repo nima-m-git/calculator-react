405f8eb03a92e4a4fb84e45525f14579878102aa
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-10-22 17:03:00 -0400

    Updates

---
+++ precache-manifest.4194a200dbaa986e58795a85bfd31f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40380d66436ddf64b2645eff36c92d54",
+    "url": "/calculator-react/index.html"
+  },
+  {
+    "revision": "cc1d7cf05be93a80cfdd",
+    "url": "/calculator-react/static/css/main.7cb9e8b0.chunk.css"
+  },
+  {
+    "revision": "e11987dbddc26825de28",
+    "url": "/calculator-react/static/js/2.b0f4a69a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-react/static/js/2.b0f4a69a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1d7cf05be93a80cfdd",
+    "url": "/calculator-react/static/js/main.897fc4b9.chunk.js"
+  },
+  {
+    "revision": "928bf717d83facf55a8d",
+    "url": "/calculator-react/static/js/runtime-main.f105f787.js"
+  }
+]);
